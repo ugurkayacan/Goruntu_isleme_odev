8b63b8cd6117513412903b326aee37a4f43854e7
ugurkayacan <59063217+ugurkayacan@users.noreply.github.com>
2022-11-18 16:16:03 +0300

    Update prewitt_filtre.py

--- prewitt_filtre.py
+++ prewitt_filtre.py
@@ -9,21 +9,22 @@ foto=cv2.imread("agac.jpg",0)
 prewit_x=np.zeros((350,545)) #0 matrisler oluşturuldu
 prewit_y=np.zeros((350,545))
 sonuc=0
-
+satir=350
+sutun=545
 K_X=[[-1,0,1],
      [-1,0,1],
      [-1,0,1]]
 
 
-for i in range(345):#out of index hatası verdiğinden dolayı satır ve sütun değerlerinde değişikliğe gidildi.
-    for j in range(535):
+for i in range(satir-3):#out of index hatası verdiğinden dolayı satır ve sütun değerlerinde değişikliğe gidildi.
+    for j in range(sutun-3):
         for z in range(3):
             for k in range(3):
 
                 carpimx=foto[i+z][j+k]*K_X[z][k]
                 sonuc+=carpimx
 
-        prewit_x[i][j] = sonuc
+        prewit_x[i+1][j+1] = sonuc
         carpim = 0;
         sonuc = 0;
 
@@ -33,15 +34,15 @@ K_Y=[[1,1,1],
      [-1,-1,-1]]
 carpim = 0;
 sonuc = 0;
-for i in range(345):
-    for j in range(543):
+for i in range(satir-3):
+    for j in range(sutun-3):
         for z in range(3):
             for k in range(3):
 
                 carpimy=foto[i+z][j+k]*K_Y[z][k]
                 sonuc+=carpimy
 
-        prewit_y[i][j]=sonuc
+        prewit_y[i+1][j+1]=sonuc
         carpim = 0;
         sonuc = 0;
 
