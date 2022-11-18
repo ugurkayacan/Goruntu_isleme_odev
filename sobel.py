9f1565d886dfa2aa28c11e2f3d9430a1e9b59821
ugurkayacan <59063217+ugurkayacan@users.noreply.github.com>
2022-11-18 16:17:53 +0300

    Update sobel.py

--- sobel.py
+++ sobel.py
@@ -16,38 +16,39 @@ foto=cv2.imread("agac.jpg",0)
 sobel_x=np.zeros((350,545))
 sobel_y=np.zeros((350,545))
 sonuc=0
-
+satir=350
+sutun=545
 K_X=[[-1,0,1],
      [-2,0,2],
      [-1,0,1]]
 
 
-for i in range(345):
-    for j in range(535):
+for i in range(satir-3):
+    for j in range(sutun-3):
         for z in range(3):
             for k in range(3):
 
                 carpimx=foto[i+z][j+k]*K_X[z][k]
                 sonuc+=carpimx
 
-        sobel_x[i][j] = sonuc
+        sobel_x[i+1][j+1] = sonuc
         carpim = 0;
         sonuc = 0;
-
+#Y
 
 K_Y=[[1,2,1],
      [0,0,0],
      [-1,-2,-1]]
 
-for i in range(345):# out of index hatası verdiği için satır sütun değerleri bir miktar geriye çekildi.
-    for j in range(543):
+for i in range(satir-3):
+    for j in range(sutun-3):
         for z in range(3):
             for k in range(3):
 
                 carpimy=foto[i+z][j+k]*K_Y[z][k]
                 sonuc+=carpimy
 
-        sobel_y[i][j]=sonuc
+        sobel_y[i+1][j+1]=sonuc
         carpim = 0;
         sonuc = 0;
 
